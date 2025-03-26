6fcb45354576bccfbc24811348e454e605aeb8fd
IGNA46img <img.872122@gmail.com>
2025-03-26 19:54:55 +0100

    Ejercicio terminado

---
+++ index.js
@@ -0,0 +1,77 @@
+const Coche ={
+    marca: '',
+    modelo: '',
+    matricula: ''
+}
+const Casa ={
+    codPostal: '',
+    calle: '',
+    portal: '',
+    piso: ''
+}
+const FullStackDeveloper ={
+    lenguajes: [],
+    proyectos: []
+}
+const Perro ={
+    nombre: '',
+    raza: '',
+    color: '',
+    edad: '',
+    ladrar: function(){
+        return console.log('¡Guau!');
+    },
+    popo: function(){
+        return Math.random()*3;     
+    }
+}
+marcaPortatil2 = Portatil.marca
+marcaPortatil = Portatil.marca
+grupos = Concierto.grupos
+const RGB = [rojo=Led.rojo,verde=Led.verde,azul=Led.azul]
+Portatil.modelo = 'P345'
+Concierto.cartelera.push("Guns N' Roses")
+Concierto.fecha = new Date()
+Impresora.imprimiendo = {
+    nombreArchivo: '',
+    copias: '',
+    numPaginas: ''
+}
+const Noticia = {
+    titular: '',
+    cuerpo: ''
+}
+const Persona = {
+    nombre: '',
+    apellidos: '',
+    edad: ''
+}
+const Avion = {
+    numPasajeros: '',
+    despegar: function(){
+        return console.log('despegando');
+    },
+    volar: function(){
+        return console.log('llegando al destino');
+    },
+    aterrizar: function(){
+        return console.log('aterrizando');
+    }
+}
+const Paquete = {
+    contenido: []
+}
+const Pais = {
+    numHabitantes: '',
+    continente: '',
+    gentilicio: ''
+}
+codError = O_Error.codigo
+integrantes = Grupo.integrantes
+nivelesTinta = Impresora.tinta
+pixeles = Pantalla.pixeles
+especificaciones = Movil['especificaciones']
+Grupo.numIntegrantes = 5
+Pantalla.dimensiones = '1920x1080'
+Led.encendido = !Led.encendido
+Movil.temperatura = '20º'
